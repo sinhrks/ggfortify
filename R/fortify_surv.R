f558db8df842d28996a4818cd0a70dab3c74995a
sinhrks <sinhrks@gmail.com>
2015-03-23 22:56:32 +0900

    BUG: fortify.survfit fails with no explanatory variable

--- R/fortify_surv.R
+++ R/fortify_surv.R
@@ -5,11 +5,11 @@
 #' @return data.frame
 #' @aliases fortify.survfit.cox
 #' @examples
-#' d.survfit <- survival::survfit(survival::Surv(time, status) ~ sex, data = survival::lung)
-#' fortify(d.survfit)
-#'
-#' d.coxph <- survival::coxph(survival::Surv(time, status) ~ sex, data = survival::lung)
-#' fortify(survival::survfit(d.coxph))
+#' library(survival)
+#' fortify(survfit(Surv(time, status) ~ sex, data = lung))
+#' fortify(survfit(Surv(time, status) ~ 1, data = lung))
+#' fortify(survfit(coxph(Surv(time, status) ~ sex, data = lung)))
+#' fortify(survfit(coxph(Surv(time, status) ~ 1, data = lung)))
 #' @export
 fortify.survfit <- function(model, data = NULL, ...) {
   d <- data.frame(time = model$time,
@@ -22,9 +22,11 @@ fortify.survfit <- function(model, data = NULL, ...) {
                   lower = model$lower)
 
   if (is(model, 'survfit.cox')) {
-    d <- cbind(d, data.frame(cumhaz = model$cumhaz))
+    d <- cbind_wraps(d, data.frame(cumhaz = model$cumhaz))
   } else if (is(model, 'survfit')) {
-    d <- cbind(d, data.frame(strata = rep(names(model$strata), model$strata)))
+    if ('strata' %in% names(model)) {
+      d <- cbind_wraps(d, data.frame(strata = rep(names(model$strata), model$strata)))
+    }
   } else {
     stop(paste0('Unsupported class for fortify.survfit: ', class(model)))
   }
@@ -43,19 +45,20 @@ fortify.survfit <- function(model, data = NULL, ...) {
 #' @param surv.shape point shape survival curve
 #' @inheritParams plot_confint
 #' @param censor Logical flag indicating whether to plot censors
-#' @param censor.shape Shape for censors
-#' @param censor.size Size for censors
+#' @param censor.colour colour for censors
+#' @param censor.size size for censors
+#' @param censor.alpha alpha for censors
+#' @param censor.shape shape for censors
 #' @inheritParams post_autoplot
 #' @param ... other arguments passed to methods
 #' @return ggplot
 #' @aliases autoplot.survfit.cox
 #' @examples
-#' d.survfit <- survival::survfit(survival::Surv(time, status) ~ sex, data = survival::lung)
-#' autoplot(d.survfit)
-#' autoplot(d.survfit, conf.int = FALSE, censor = FALSE)
-#'
-#' d.coxph <- survival::coxph(survival::Surv(time, status) ~ sex, data = survival::lung)
-#' autoplot(survival::survfit(d.coxph))
+#' library(survival)
+#' autoplot(survfit(Surv(time, status) ~ sex, data = lung))
+#' autoplot(survfit(Surv(time, status) ~ 1, data = lung))
+#' autoplot(survfit(Surv(time, status) ~ sex, data=lung), conf.int = FALSE, censor = FALSE)
+#' autoplot(survfit(coxph(Surv(time, status) ~ sex, data = lung)))
 #' @export
 autoplot.survfit <- function(object,
                              surv.geom = 'line',
@@ -64,8 +67,8 @@ autoplot.survfit <- function(object,
                              conf.int = TRUE,
                              conf.int.colour = '#0000FF', conf.int.linetype = 'none',
                              conf.int.fill = '#000000', conf.int.alpha = 0.3,
-                             censor = TRUE,
-                             censor.shape = '+', censor.size = 3,
+                             censor = TRUE, censor.colour = NULL, censor.size = 3,
+                             censor.alpha = NULL, censor.shape = '+',
                              xlim = c(NA, NA), ylim = c(NA, NA), log = "",
                              main = NULL, xlab = NULL, ylab = NULL, asp = NULL,
                              ...) {
@@ -75,33 +78,25 @@ autoplot.survfit <- function(object,
   if (is.null(surv.colour) & ('strata' %in% colnames(plot.data))) {
       surv.colour <- 'strata'
   }
+  if (missing(conf.int.fill) & !is.null(surv.colour)) {
+    conf.int.fill <- surv.colour
+  }
 
   geomfunc <- get_geom_function(surv.geom, allowed = c('line', 'point'))
 
   p <- ggplot2::ggplot(data = plot.data, mapping = ggplot2::aes_string(x = 'time', y = 'surv')) +
     ggplot2::scale_y_continuous(labels = scales::percent)
-  if ('strata' %in% names(plot.data)) {
-    p <- p + geom_factory(geomfunc, plot.data,
-                          colour = surv.colour, size = surv.size, linetype = surv.linetype,
-                          alpha = surv.alpha, fill = surv.fill, shape = surv.shape)
-    p <- plot_confint(p, data = plot.data, conf.int = conf.int,
-                      conf.int.colour = conf.int.colour,
-                      conf.int.linetype = conf.int.linetype,
-                      conf.int.fill = surv.colour, conf.int.alpha = conf.int.alpha)
-  } else {
-    p <- p + geom_factory(geomfunc, plot.data,
-                          colour = surv.colour, size = surv.size, linetype = surv.linetype,
-                          alpha = surv.alpha, fill = surv.fill, shape = surv.shape)
-    p <- plot_confint(p = p, data = plot.data, conf.int = conf.int,
-                      conf.int.colour = conf.int.colour,
-                      conf.int.linetype = conf.int.linetype,
-                      conf.int.fill = conf.int.fill, conf.int.alpha = conf.int.alpha)
-  }
-
+  p <- p + geom_factory(geomfunc, plot.data,
+                        colour = surv.colour, size = surv.size, linetype = surv.linetype,
+                        alpha = surv.alpha, fill = surv.fill, shape = surv.shape)
+  p <- plot_confint(p, data = plot.data, conf.int = conf.int,
+                    conf.int.colour = conf.int.colour,
+                    conf.int.linetype = conf.int.linetype,
+                    conf.int.fill = conf.int.fill, conf.int.alpha = conf.int.alpha)
   if (censor) {
-    p <- p + ggplot2::geom_point(data = plot.data[plot.data$n.censor > 0, ],
-                                 ggplot2::aes_string(y = 'surv'),
-                                 shape = censor.shape, size = censor.size)
+    p <- p + geom_factory(geom_point, plot.data[plot.data$n.censor > 0, ],
+                          colour = censor.colour, size = censor.size,
+                          alpha = censor.alpha, shape = censor.shape)
   }
   p <- post_autoplot(p = p, xlim = xlim, ylim = ylim, log = log,
                      main = main, xlab = xlab, ylab = ylab, asp = asp)
--- tests/testthat/test-surv.R
+++ tests/testthat/test-surv.R
@@ -2,19 +2,46 @@ library(survival)
 
 context('test survival')
 
-test_that('fortify.survfit works for survival::lung', {
-  d.survfit <- survival::survfit(survival::Surv(time, status) ~ sex, data = survival::lung)
+test_that('fortify.survfit works for lung', {
+  d.survfit <- survfit(Surv(time, status) ~ sex, data = lung)
   fortified <- ggplot2::fortify(d.survfit)
 
   expect_equal(is.data.frame(fortified), TRUE)
   expected_names <- c('time', 'n.risk', 'n.event', 'n.censor', 'surv',
                       'std.err', 'upper', 'lower', 'strata')
   expect_equal(names(fortified), expected_names)
+  expect_equal(dim(fortified), c(206, 9))
+
+  d.survfit <- survfit(Surv(time, status) ~ 1, data = lung)
+  fortified <- ggplot2::fortify(d.survfit)
+
+  expect_equal(is.data.frame(fortified), TRUE)
+  expected_names <- c('time', 'n.risk', 'n.event', 'n.censor', 'surv',
+                      'std.err', 'upper', 'lower')
+  expect_equal(names(fortified), expected_names)
+  expect_equal(dim(fortified), c(186, 8))
 })
 
-test_that('fortify.survfit.cox works for survival::lung', {
-  d.coxph <- survival::coxph(survival::Surv(time, status) ~ sex, data = survival::lung)
-  fortified <- ggplot2::fortify(survival::survfit(d.coxph))
+test_that('fortify.survfit works for simple data', {
+  tdata <- data.frame(time = c(1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4),
+                      status = rep(c(1, 0, 2), 4),
+                      n = c(12, 3, 2, 6, 2, 4, 2, 0, 2, 3, 3, 5))
+  fit  <- survfit(Surv(time, time, status, type='interval') ~1, data = tdata, weight = n)
+  fortified <- fortify(fit)
+  expected = data.frame(time = c(1, 2, 3, 4),
+                        n.risk = c(44.000000000, 20.652979445, 9.318098786, 6.634779353),
+                        n.event = c(20.347020555, 9.334880659, 2.683319433, 3.634779353),
+                        n.censor = c(3, 2, 0, 3),
+                        surv = c(0.53756771467, 0.29459403255, 0.20976021500, 0.09484575319),
+                        std.err = c(0.1398238147, 0.2438966932, 0.3207623423, 0.5343228432),
+                        upper = c(0.7070521134, 0.4751483078, 0.3933295002, 0.2702927012),
+                        lower = c(0.40870968683, 0.18264959088, 0.11186383878, 0.03328139035))
+  expect_equal(fortified, expected)
+})
+
+test_that('fortify.survfit.cox works for lung', {
+  d.coxph <- coxph(Surv(time, status) ~ sex, data = lung)
+  fortified <- ggplot2::fortify(survfit(d.coxph))
 
   expect_equal(is.data.frame(fortified), TRUE)
   expected_names <- c('time', 'n.risk', 'n.event', 'n.censor', 'surv',
