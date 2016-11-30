332a4c088c19a1fbe1fa94d583f57d0bb8d23664
sinhrks <sinhrks@gmail.com>
2016-12-01 07:49:51 +0900

    BUG: unable to autoplot varpred

--- tests/testthat/test-vars.R
+++ tests/testthat/test-vars.R
@@ -2,7 +2,7 @@ library(vars)
 
 context('test vars')
 
-test_that('vars.varored works for Canada', {
+test_that('vars.varpred works for Canada', {
   data(Canada, package = 'vars')
   d.vselect <- vars::VARselect(Canada, lag.max = 5, type = 'const')$selection[1]
   d.var <- vars::VAR(Canada, p = d.vselect, type = 'const')
@@ -36,4 +36,17 @@ test_that('vars.varored works for Canada', {
   expect_equal(fortified$Index[1], as.Date('1980-01-01'))
   expect_equal(fortified$Index[nrow(fortified)], as.Date('2013-04-01'))
 
+})
+
+test_that('autoplot works for Canada', {
+  data(Canada, package = 'vars')
+  d.vselect <- VARselect(Canada, lag.max = 5, type = 'const')$selection[1]
+  d.var <- VAR(Canada, p = d.vselect, type = 'const')
+
+  p <- autoplot(predict(d.var, n.ahead = 50))
+  expect_true(is(p, 'ggplot'))
+
+  p <- autoplot(predict(d.var, n.ahead = 50), ts.colour = 'dodgerblue4',
+                predict.colour = 'blue', predict.linetype = 'dashed')
+  expect_true(is(p, 'ggplot'))
 })
